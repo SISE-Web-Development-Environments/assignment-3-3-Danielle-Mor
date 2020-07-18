5d4666b6b5f02010e0893db821bdf4ba64cd0845
Danielavi611 <lavida@post.bgu.ac.il>
2020-07-18 22:13:35 +0300

    added files

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="container">
+  <div class="massages">
     <h1>Four Oh Four you didn't</h1>
     <router-link to="/" exact>ET Go Home</router-link>
   </div>
 </template>
+
+<style>
+.massages {
+  padding-top: 300px;
+  color: azure;
+  text-align: center;
+  font-family: "Sofia";
+  font-size: 50px;
+  text-shadow: 2px 2px 4px #000000;
+}
+</style>
